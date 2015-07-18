3dd26aad103e0e710d297ef6446b6ce50da284fe
0pp21d607852z2c@ezweb.ne.jp <kudokansuke@yahoo.co.jp>
2015-07-18 22:57:54 +0900

    ChatServer.javaの作成 kudou

---
+++ ChatServer.java
@@ -0,0 +1,28 @@
+import java.net.*;
+import java.io.*;
+
+public class ChatServer{
+    private ServerSocket server;
+
+    public void listen(){
+        try{
+            server = new ServerSocket(18080);
+            System.out.println("Echoサーバをポート18080で起動しました．");
+            //Socket socket = server.accept();
+            System.out.println("クライアントが接続してきました．");
+
+            while(true){
+                Socket socket = server.accept();
+                ChatClientHandler handler = new ChatClientHandler(socket);
+                handler.start();
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        ChatServer echo = new ChatServer();
+        echo.listen();
+    }
+}
