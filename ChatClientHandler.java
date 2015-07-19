957caf9aa63b2a815a66cf4a563e918927bcbb38
Hila_201 <rockblack00@yahoo.co.jp>
2015-07-19 14:40:43 +0900

    NAME機能の追加

--- ChatClientHandler.java
+++ ChatClientHandler.java
@@ -5,6 +5,8 @@ public class ChatClientHandler extends Thread{
     private Socket socket;
     private BufferedReader in;
     private BufferedWriter out;
+    String name;  //ユーザの名前
+    ChatServer chat2;  //チャットサーバの参照型変数
 
     public ChatClientHandler(Socket socket){
         this.socket = socket;
@@ -23,6 +25,9 @@ public class ChatClientHandler extends Thread{
 		if(commands[0].equalsIgnoreCase("help")) {
 		    help();
 		}
+                if(commands[0].equalsIgnoreCase("name")) {
+                    name(commands[1]);
+                }
             }
         } catch(IOException e){
             e.printStackTrace();
@@ -46,6 +51,17 @@ public class ChatClientHandler extends Thread{
     //helpコマンド(使えるコマンドの種類を表示させる)
     public void help() throws IOException {
         this.send("HELP,NAME,WHOAMI,USERS,BYE,POST");
+    }
+    
+        //nameコマンド(自分の名前を設定する処理)
+    public void name(String name) throws IOException {
+        int a = chat2.changeName(name);
+        if(a == 0) {
+            this.send("this name is used");     
+        } else {
+            this.name = name;
+            this.send(name);
+        }
     }
 
     /**
