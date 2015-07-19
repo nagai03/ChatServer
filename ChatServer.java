df59bcc1e2c3a7a9c377d567e85d494e798afa8a
Hila_201 <rockblack00@yahoo.co.jp>
2015-07-19 14:56:51 +0900

    NAME機能の追加
    チャットサーバでnameコマンドが実行された場合、ChatServer.javaで名前を変更する前に
    その名前に変更可能かどうかを判定する。変更可能であればそのまま新しい名前を追加する。
    すでに使われている場合はエラーメッセージを表示する。

--- ChatServer.java
+++ ChatServer.java
@@ -3,6 +3,8 @@ import java.io.*;
 
 public class ChatServer{
     private ServerSocket server;
+    private String name2;  //古い名前を格納
+
 
     public void listen(){
         try{
@@ -19,6 +21,26 @@ public class ChatServer{
         } catch(IOException e){
             e.printStackTrace();
         }
+    }
+    
+       
+    //名前を変更する場合にその名前に変更可能かどうかを判定するメソッド
+    public int changeName(String name) {
+        for (int i=0; i<clients.size(); i++) {
+            if(namae.get(i).equals(name)){
+                //ここで名前が既に存在する場合の処理
+                return 0;
+            }
+        }
+        //ここで名前がそんざいしない場合の処理
+        //名前が存在しなければ、namaeのリストの古い名前を削除、新しい名前を追加
+        namae.remove(name2);
+        namae.add(name);
+        for (int i=0; i<namae.size(); i++) {
+	    System.out.println(namae.get(i));
+        }
+        //クライアントハンドラの方の名前も新しいものにする
+        return 1;
     }
 
     public static void main(String[] args){
